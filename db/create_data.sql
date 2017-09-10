2af8470144d5a02948666e1ad627d97d99911407
Ryan Delaney <rrdelaney@outlook.com>
2017-09-09 18:15:15 -0700

    More mock data

--- db/create_data.sql
+++ db/create_data.sql
@@ -1,1 +1,65 @@
-insert into communities (name) values ('Movies');
+insert into users values ('tu_1', 'John Smith');
+insert into users values ('tu_2', 'Elia Martell');
+
+insert into communities values ('movies', 'Movies');
+insert into communities values ('science', 'Science');
+insert into communities values ('photography', 'Photography');
+
+insert into issues (headline, published, community_id) values (
+  'Best summer movies',
+  date '2017-08-16',
+  'movies'
+);
+
+insert into issues (headline, published, community_id) values (
+  'Improve everything',
+  date '2017-08-04',
+  'photography'
+);
+
+insert into issues (headline, published, community_id) values (
+  'Be better at pictures!',
+  date '2017-09-08',
+  'photography'
+);
+
+insert into posts (title, content, community_id, author_id) values (
+  'Top-grossing 2017 summer movies',
+  'Huge list here',
+  'movies',
+  'tu_2'
+);
+
+insert into posts (title, content, community_id, author_id) values (
+  'My favorite scenes in Avatar',
+  'Huge list here',
+  'movies',
+  'tu_2'
+);
+
+insert into posts (title, content, community_id, author_id) values (
+  'Highest rated movies ever',
+  'Huge list here',
+  'movies',
+  'tu_1'
+);
+
+insert into posts (title, content, community_id, author_id) values (
+  'Intro to Photography',
+  'This is how we do it',
+  'photography',
+  'tu_1'
+);
+
+insert into posts (title, content, community_id, author_id) values (
+  'Empty Space',
+  'This is a guide on empty space',
+  'photography',
+  'tu_1'
+);
+
+insert into issue_posts values (1, 1);
+insert into issue_posts values (1, 2);
+insert into issue_posts values (1, 3);
+insert into issue_posts values (2, 4);
+insert into issue_posts values (3, 5);
--- db/create_schema.sql
+++ db/create_schema.sql
@@ -1,4 +1,4 @@
-drop table if exists articles cascade;
+drop table if exists issue_posts cascade;
 drop table if exists posts cascade;
 drop table if exists issues cascade;
 drop table if exists communities cascade;
@@ -10,24 +10,26 @@ create table users (
 );
 
 create table communities (
-  id serial primary key,
+  id varchar(50) primary key,
   name varchar(255) not null
 );
 
 create table issues (
   id serial primary key,
-  name varchar(255) not null,
-  community_id serial references communities(id)
+  headline varchar(255) not null,
+  published date not null,
+  community_id varchar(50) references communities(id)
 );
 
 create table posts (
   id serial primary key,
   title varchar(255) not null,
   content text not null,
+  community_id varchar(50) references communities(id),
   author_id varchar(50) references users(id)
 );
 
-create table articles (
+create table issue_posts (
   issue_id serial references issues(id),
   post_id serial references posts(id),
   primary key (issue_id, post_id)
